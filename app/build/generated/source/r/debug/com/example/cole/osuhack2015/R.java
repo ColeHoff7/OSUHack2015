f76a18fc509ecb8f26c545527180d148e9d02911
Cole Hoffbauer <colehoff7@gmail.com>
2015-11-14 17:10:17 -0500

    Just made an initial welcome screen along with a button that takes you to the preferences screen

--- app/build/generated/source/r/debug/com/example/cole/osuhack2015/R.java
+++ app/build/generated/source/r/debug/com/example/cole/osuhack2015/R.java
@@ -15,10 +15,12 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int button=0x7f080002;
-        public static final int textView=0x7f080001;
-        public static final int welcome=0x7f080000;
+        public static final int action_settings=0x7f080005;
+        public static final int button=0x7f080004;
+        public static final int ll=0x7f080001;
+        public static final int prefLayout=0x7f080000;
+        public static final int textView=0x7f080003;
+        public static final int welcome=0x7f080002;
     }
     public static final class layout {
         public static final int activity_initial_preferences=0x7f030000;
--- app/src/main/java/com/example/cole/osuhack2015/InitialPreferences.java
+++ app/src/main/java/com/example/cole/osuhack2015/InitialPreferences.java
@@ -5,10 +5,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.Toast;
 
 
 public class InitialPreferences extends Activity {
+
+    CheckBox food;
+    CheckBox night;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +26,18 @@ public class InitialPreferences extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial_preferences);
         Intent intent = getIntent();
+
+        final LinearLayout ll = (LinearLayout) findViewById(R.id.ll);
+        ll.setOrientation(LinearLayout.VERTICAL);
+        food = new CheckBox(this);
+        food.setText("Food");
+        ll.addView(food);
+
+        night = new CheckBox(this);
+        night.setText("Night Life");
+        ll.addView(night);
+
+        listener(ll);
     }
 
 
@@ -31,7 +53,32 @@ public class InitialPreferences extends Activity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    public void listener(final LinearLayout ll){
+        //CheckBox food = (CheckBox) findViewById(R.id.food);
+        food.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //is food checked?
+                if (((CheckBox) v).isChecked()) {
+                    //Toast.makeText(InitialPreferences.this, "Food Added", Toast.LENGTH_LONG).show();
+                    foodClick(ll);
+                }
+
+            }
+        });
+    }
+
+    public void foodClick(LinearLayout ll){
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.leftMargin = 100;
+
+        CheckBox burger = new CheckBox(this);
+        burger.setText("Burgers");
+        ll.addView(burger, params);
+
     }
 }
