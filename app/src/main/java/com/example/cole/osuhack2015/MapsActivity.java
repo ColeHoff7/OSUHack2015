fe6fbcadb15cf4c5ffd5c89755b83492ec9c8907
Kwangju Kim <kimk3@miamioh.edu>
2015-11-15 09:48:34 -0500

    further improvements pseudocode posted

--- app/src/main/java/com/example/cole/osuhack2015/MapsActivity.java
+++ app/src/main/java/com/example/cole/osuhack2015/MapsActivity.java
@@ -38,7 +38,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * we just add a marker near Columbus, Ohio, USA.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -46,9 +46,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+	double latitude = 39.9833;
+	double longitude = -82.9833;
 
-        // Add a marker in Sydney and move the camera
-        LatLng columbus = new LatLng(39.9833, -82.9833);
+        // Add a marker in Columbus and move the camera
+	GPSTracker tracker = new GPSTracker(this);
+
+	// If the location tracker is open (either GPS or WiFi)
+	// Returns the current location
+	if (tracker.canGetLocation()) {
+		latitude = traacker.getLatitude();
+		longitude = tracker.getLongitude();
+	}
+        LatLng columbus = new LatLng(latitude, longitude);
         mMap.addMarker(new MarkerOptions().position(new LatLng(39.975, -82.9733)).title("Marker in Columbus"));
         mMap.addMarker(new MarkerOptions().position(new LatLng(39.971, -82.9843)).title("Marker in Columbus"));
         mMap.addMarker(new MarkerOptions().position(new LatLng(39.983,-82.9913)).title("Marker in Columbus"));
@@ -56,7 +66,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(columbus, 11.0f));
 
 
+	/*
+	 * Use Google Search API
+	 * Generate the array
+	 * Stores queries based on users' pre-defined preferences
+	 * List the top 20 (will support more than 20 at some pt)
+	 *
+	 * HashMap<K, V> preference = (user_object).getPref();
+	 * for (pref : hashmap) {
+	 *    $post(pref) => GOogle Search
+	 *    $get => geoloc of certain attractions
+	 * }
+	 *
+	 * showResult();
+	 *
+	 *
+	 */
+
     }
 
+// FURTHER IMPROVEMENTS
+
+    // Some push notification stuff comes here?
 
 }
